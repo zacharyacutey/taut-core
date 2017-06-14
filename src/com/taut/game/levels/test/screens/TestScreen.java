1b71d7695be2d74aeed4d4127f2234b0a7cb4412
Garrett Smith <garrett_smith@aol.com>
2017-06-13 23:12:58 -0400

    Load all entities

--- src/com/taut/game/levels/test/screens/TestScreen.java
+++ src/com/taut/game/levels/test/screens/TestScreen.java
@@ -16,6 +16,13 @@ import com.taut.game.GlobalData;
 import com.taut.game.levels.test.TestData;
 import com.taut.game.models.NPC;
 import com.taut.game.models.NPCGenerator;
+import com.taut.game.models.items.Armor;
+import com.taut.game.models.items.ArmorGenerator;
+import com.taut.game.models.items.Consumable;
+import com.taut.game.models.items.ConsumableGenerator;
+import com.taut.game.models.items.Weapon;
+import com.taut.game.models.items.WeaponGenerator;
+import com.taut.game.objects.FolderContents;
 import com.taut.game.objects.Player;
 import com.taut.game.objects.SpriteMovement.Direction;
 import com.taut.game.objects.TautAnimatedSprite;
@@ -33,11 +40,25 @@ public class TestScreen extends ScreenAdapter {
 	SpriteBatch spriteBatch;
 	ShapeRenderer shapeRenderer;
 	Player player;
-	ArrayList<NPC> npcs = new ArrayList<>();
 	String levelName;
 	String screenName;
+	
+	// loading all the spawnable entities
+	ArrayList<NPC> npcs = new ArrayList<>();
 	NPCGenerator npcGenerator = new NPCGenerator();
+	FolderContents npcFolderContents = new FolderContents("NPC");
 
+	ArrayList<Weapon> weapons = new ArrayList<>();
+	WeaponGenerator weaponGenerator = new WeaponGenerator();
+	FolderContents weaponFolderContents = new FolderContents("weapons");
+
+	ArrayList<Consumable> consumables = new ArrayList<>();
+	ConsumableGenerator consumableGenerator = new ConsumableGenerator();
+	FolderContents consumableFolderContents = new FolderContents("consumables");
+	
+	ArrayList<Armor> armor = new ArrayList<>();
+	ArmorGenerator armorGenerator = new ArmorGenerator();
+	FolderContents armorFolderContents = new FolderContents("armor");
 	
 	public TestScreen(final Taut game, String levelName, String screenName)
 	{
@@ -45,6 +66,11 @@ public class TestScreen extends ScreenAdapter {
 		this.game = game;
 		this.levelName = levelName;
 		this.screenName = screenName;
+		
+		npcFolderContents.generateAndStore(npcGenerator, npcs);
+		weaponFolderContents.generateAndStore(weaponGenerator, weapons);
+		consumableFolderContents.generateAndStore(consumableGenerator, consumables);
+		armorFolderContents.generateAndStore(armorGenerator, armor);		
 	}
 	
 	@Override
@@ -52,7 +78,6 @@ public class TestScreen extends ScreenAdapter {
 	{
 		map = TestData.getMainMap();
 		animatedSprite = GlobalData.getPlayerWalkAnimation(); 
-		npcs = npcGenerator.generateAllNPCs();
 		camera = new TautCamera(16);
 		mapRenderer = new TautOrthogonalTiledMapRenderer(map, 1f/16f);
 		spriteBatch = new SpriteBatch();
